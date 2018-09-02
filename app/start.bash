8a2b38a04217ac53f33a7d8dca2a384e88978f32
Andrew Phillips <theasp@gmail.com>
2018-09-02 11:35:41 -0300

    Don't overwrite slurm.conf

--- app/start.bash
+++ app/start.bash
@@ -5,10 +5,11 @@ set -xeo pipefail
 export HOSTNAME=$(hostname)
 export CLUSTERNAME=$(hostname)
 
-test -e /etc/munge/munge.key || /sbin/create-munge-key
 
 mkdir -p /var/run/munge
 chown munge:munge /var/run/munge/
 
-envsubst < /app/slurm.conf.envsubst > /etc/slurm-llnl/slurm.conf
+test -e /etc/munge/munge.key || /sbin/create-munge-key
+test -e /etc/slurm-llnl/slurm.conf || envsubst < /app/slurm.conf.envsubst > /etc/slurm-llnl/slurm.conf
+
 /usr/bin/supervisord --nodaemon --configuration /app/supervisord.conf
