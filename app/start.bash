dc6d4f36e6287ea014c3fac4ec4a142ccefdcf04
Andrew Phillips <theasp@gmail.com>
2018-09-02 14:13:29 -0300

    Use default values for pid and logfile locations

--- app/start.bash
+++ app/start.bash
@@ -8,6 +8,8 @@ export CLUSTERNAME=$(hostname)
 
 mkdir -p /var/run/munge
 mkdir -p /var/log/supervisor
+mkdir -p /var/log/slurm-llnl
+mkdir -p /var/run/slurm-llnl
 chown munge:munge /var/run/munge/
 
 test -e /etc/munge/munge.key || /sbin/create-munge-key
